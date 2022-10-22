0e1788117170e1e751996c5e764b30a8e561a8d8
sushi057 <champsuvash@gmail.com>
2022-10-22 15:25:42 +0545

    show category

--- src/displayCats.js
+++ src/displayCats.js
@@ -6,19 +6,31 @@ const displayCategories = ( () => {
     let importantTasks = [];
     let myDayTasks = [];
 
+    const catMyDay = categoryFactory(document.querySelector('.my-day'), myDayTasks);
+    const catImortant = categoryFactory(document.querySelector('.important'), importantTasks);
+    const catCompleted = categoryFactory(document.querySelector('.completed'), completedTasks);
+    const catDue = categoryFactory(document.querySelector('.due-tasks'), dueTasks);
+    const catAll = categoryFactory(document.querySelector('.all-tasks'), allTasks);
+
     const taskList = document.querySelector('.tasks-list');
-        
-    const showMyDay = () => {
-        const myDayButton = document.querySelector('.my-day');
-
-        myDayButton.addEventListener('click', () => {
+       
+    const showCategory = (category) => {
+        category.element.addEventListener('click', () => {
             taskList.innerHTML = '';
-
+            taskList.appendChild(arrayToTasks(category.array));
         })
-
-        taskList.appendChild(arrayToTasks(myDayTasks));
-
     }
+
+    // const showMyDay = () => {
+    //     const myDayButton = document.querySelector('.my-day');
+
+    //     myDayButton.addEventListener('click', () => {
+    //         taskList.innerHTML = '';
+
+    //     })
+    //     taskList.appendChild(arrayToTasks(myDayTasks));
+
+    // }
 
     const arrayToTasks = (array) => {
         array.forEach(element => {
@@ -35,4 +47,14 @@ const displayCategories = ( () => {
             taskList.appendChild(newTask);
         });
     }
-})
+
+    //Categories Factory Function
+    const categoryFactory = (element, array) => {
+        return {element, array};
+    }
+
+    return {
+        showCategory,
+        
+    }
+})();
