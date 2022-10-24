ae31a681cf283bcd7131ccf9a519308eeba974ec
sushi057 <champsuvash@gmail.com>
2022-10-24 10:21:55 +0545

    idek

--- src/manageTasks.js
+++ src/manageTasks.js
@@ -11,26 +11,25 @@ class Task {
 
     displayTask(){
         const taskList = document.querySelector('.tasks-list');
+
         const newTask = document.createElement('div');
         newTask.classList.add('task');
 
         newTask.innerHTML = `
-        <input type="checkbox" class="check">
-                <div class="task-detail">
-                    <p class="task-title">${this.name}</p>
-                    <p class="task-date">${this.date}</p>
-                </div>
-                `
+            <input type="checkbox" class="check">
+            <div class="task-detail">
+                <p class="task-title">${this.name}</p>
+                <p class="task-date">${this.date}</p>
+            </div>
+            `
 
-                taskList.appendChild(newTask)
+        taskList.appendChild(newTask)
     }
 
     removeTask(){
         console.log('todo');
     }
-
 }
-
 
 const taskList = document.querySelector('.tasks-list');
 const dialogBox = document.querySelector('dialog');
@@ -38,10 +37,41 @@ const dialogBox = document.querySelector('dialog');
 
 //Adding a task after user presses 'Enter'
 const inputBar = document.querySelector('.add-task');
+
 inputBar.addEventListener('keydown', (e) => {
     if (e.key == 'Enter'){
         if (e.target.value !== ''){
-            takeTaskDetails(e);
+            dialogBox.showModal();
+            document.querySelector('#button').addEventListener('click', () => {
+                dialogBox.close();
+            })
+        
+            document.querySelector('#task-name').value = e.target.value;
+            dialogBox.addEventListener('submit', () => {
+            
+                const newTaskName = document.querySelector('#task-name').value;
+                const newTaskDate = document.querySelector('#task-date').value;
+                const newTaskDesc = document.querySelector('#task-description').value;
+                const newTaskImp =  checkTaskImportance();
+            
+                const newTask = new Task(newTaskName, newTaskDate, newTaskDesc, newTaskImp);
+        
+                console.log(catMyDay.array);
+
+                catAll.array.push(newTask);
+                catMyDay.array.push(newTask);
+        
+                if(newTaskImp == true){
+                    catImportant.array.push(newTask);
+                }
+
+                console.log(catMyDay.array);
+        
+                newTask.displayTask();
+                catMyDay.displayCats();
+                document.querySelector('form').reset();
+            })
+
             e.target.value = null;
         }
         else{
@@ -50,37 +80,32 @@ inputBar.addEventListener('keydown', (e) => {
     }
 })
 
-//When task is clicked, a dialog appears asking for further details
-function takeTaskDetails(e){
 
-    dialogBox.showModal();
-    document.querySelector('#button').addEventListener('click', () => {
-        dialogBox.close();
-    })
-
-    document.querySelector('#task-name').value = e.target.value;
-    dialogBox.addEventListener('submit', () => {
+function removeTaskFromList(){
+    const taskCheckBox = document.querySelectorAll('.check');
     
-        let newTaskName = document.querySelector('#task-name').value;
-        let newTaskDate = document.querySelector('#task-date').value;
-        let newTaskDesc = document.querySelector('#task-description').value;
-        let newTaskImp =  checkTaskImportance();
-    
-        let newTask = new Task(newTaskName, newTaskDate, newTaskDesc, newTaskImp);
-
-        catAll.array.push(newTask);
-        catMyDay.array.push(newTask);
-
-        if(newTaskImp == true){
-            catImportant.array.push(newTask);
-        }
-        
-        document.querySelector('form').reset();
-
-        newTask.displayTask();
+    taskCheckBox.forEach((task) => {
+        task.addEventListener('change', () => {
+            if (task.checked) {
+                console.log(task);
+                task.parentNode.parentNode.removeChild(task.parentNode);
+            }   
+        })
     })
-
 }
+
+function checkTaskImportance(){
+    if (document.querySelector('#task-importance').checked){
+        document.querySelector('#task-importance').checked = false;
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+removeTaskFromList();
+
 
 // Future Feature //
 
@@ -115,29 +140,3 @@ function viewTaskDetails(){
         })
     })
 }
-
-
-function removeTaskFromList(){
-    const taskCheckBox = document.querySelectorAll('.check');
-    
-    taskCheckBox.forEach((task) => {
-        task.addEventListener('change', () => {
-            if (task.checked) {
-                console.log(task);
-                task.parentNode.parentNode.removeChild(task.parentNode);
-            }   
-        })
-    })
-}
-
-function checkTaskImportance(){
-    if (document.querySelector('#task-importance').checked){
-        document.querySelector('#task-importance').checked = false;
-        return true;
-    }
-    else{
-        return false;
-    }
-}
-
-removeTaskFromList();
